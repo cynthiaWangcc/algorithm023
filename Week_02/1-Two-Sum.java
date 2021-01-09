9e596844f28ce93fe4851a829f653ff71c10a79f
cynthiaWangcc <wcc19872002@hotmail.com>
2021-01-09 23:03:58 +0800

    1-Two-Sum

---
+++ Week_02/1-Two-Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = hashMap.get(target - nums[i]);
+            }
+
+            hashMap.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
